0e15c2aaa4df4e3264c42a32d31f88d5a636c99c
tony-coder <1606771552@qq.com>
2022-09-10 18:14:45 +0800

    eureka注册中心

---
+++ microservice-eureka/src/main/java/com/bfx/springcloud/microserviceeureka/MicroserviceEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.bfx.springcloud.microserviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// EurekaServer服务器端启动类,接受其它微服务注册进来
+@EnableEurekaServer
+public class MicroserviceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEurekaApplication.class, args);
+    }
+
+}
